8ce4f9045a485114b40c5fba115baa154a96f3f6
corgan <corganhejijun@qq.com>
2018-09-03 22:43:37 +0800

    add celeba trans

---
+++ trans_celeba.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from src.util import transFromDir
+
+DATASET = "datasets\\celeba\\celeba_identified"
+ALIGNED_DATA = "datasets\\celeba_aligned"
+OUT_DIR = "datasets\\celeba_trans"
+SHAPE_MODEL = "models\\shape_predictor_68_face_landmarks.dat"
+
+EIGEN_PATH = "datasets\\eigen_face.jpg"
+OUT_SIZE = 256
+
+transFromDir(DATASET, ALIGNED_DATA, OUT_DIR, SHAPE_MODEL, EIGEN_PATH, OUT_SIZE)
