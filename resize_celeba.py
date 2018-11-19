892236fcd716bbc7d937c4c4a2f05dd9a9c001ee
corganhejijun <corganhejijun@gmail.com>
2018-11-19 10:21:45 +0800

    256 to 128

--- resize_celeba.py
+++ resize_celeba.py
@@ -4,6 +4,6 @@ DATA_SET = "datasets/celeba_trans"
 OUT_DIR = "datasets/celeba_resize_for_train"
 MARGIN_FILE = "datasets/celeba_resize_margin.txt"
 
-IMAGE_SIZE = 256
+IMAGE_SIZE = 128
 
 getTrainImg(DATA_SET, IMAGE_SIZE, OUT_DIR, MARGIN_FILE) 
