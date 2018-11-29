c78a802c3ec1cdd11cd3be5f0de28f95fa462fa5
corganhejijun <corganhejijun@gmail.com>
2018-11-29 11:19:13 +0800

    residual block unequal

--- src/pix2pix/model.py
+++ src/pix2pix/model.py
@@ -387,9 +387,9 @@ class pix2pix(object):
         return tf.nn.relu(rb_sum)
 
     def residual_block_2_128(self, image):
-        rb1 = self.g_bn_rb2_1_128(conv2d(image, self.gf_dim * 2, d_h=1, d_w=1, name='g_rb_128_conv_rb2_1'))
+        rb1 = self.g_bn_rb2_1_128(conv2d(image, self.gf_dim, d_h=1, d_w=1, name='g_rb_128_conv_rb2_1'))
         rb1 = tf.nn.relu(rb1)
-        rb2 = self.g_bn_rb2_2_128(conv2d(rb1, self.gf_dim * 2, d_h=1, d_w=1, name='g_rb_128_conv_rb2_2'))
+        rb2 = self.g_bn_rb2_2_128(conv2d(rb1, self.gf_dim, d_h=1, d_w=1, name='g_rb_128_conv_rb2_2'))
         rb_sum = tf.add(image, rb2, name='g_rb_128_add_rb2')
         return tf.nn.relu(rb_sum)
 
@@ -412,48 +412,61 @@ class pix2pix(object):
             print("##############image shape is {0}".format(image.get_shape()))
             # image is (128 x 128 x input_c_dim)
             e1 = conv2d(image, self.gf_dim, name='g_e1_conv')
+            print("##############e1 shape is {0}".format(e1.get_shape()))
             # e1 is (64 x 64 x self.gf_dim)
             e2 = self.g_bn_e2(conv2d(lrelu(e1), self.gf_dim*2, name='g_e2_conv'))
+            print("##############e2 shape is {0}".format(e2.get_shape()))
             # e2 is (32 x 32 x self.gf_dim*2)
             e3 = self.g_bn_e3(conv2d(lrelu(e2), self.gf_dim*4, name='g_e3_conv'))
+            print("##############e3 shape is {0}".format(e3.get_shape()))
             # e3 is (16 x 16 x self.gf_dim*4)
             e4 = self.g_bn_e4(conv2d(lrelu(e3), self.gf_dim*8, name='g_e4_conv'))
+            print("##############e4 shape is {0}".format(e4.get_shape()))
             # e4 is (8 x 8 x self.gf_dim*8)
             e5 = self.g_bn_e5(conv2d(lrelu(e4), self.gf_dim*8, name='g_e5_conv'))
+            print("##############e5 shape is {0}".format(e5.get_shape()))
             # e5 is (4 x 4 x self.gf_dim*8)
             e6 = self.g_bn_e6(conv2d(lrelu(e5), self.gf_dim*8, name='g_e6_conv'))
+            print("##############e6 shape is {0}".format(e6.get_shape()))
             # e6 is (2 x 2 x self.gf_dim*8)
             e7 = self.g_bn_e7(conv2d(lrelu(e6), self.gf_dim*8, name='g_e7_conv'))
+            print("##############e7 shape is {0}".format(e7.get_shape()))
             # e7 is (1 x 1 x self.gf_dim*8)
 
             self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e7),
                 [self.batch_size, s128, s128, self.gf_dim*8], name='g_d1', with_w=True)
             d1 = self.g_bn_d1(self.d1)
+            print("##############d1 shape is {0}".format(d1.get_shape()))
             # d1 is (2 x 2 x self.gf_dim*8*2)
 
             self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
                 [self.batch_size, s64, s64, self.gf_dim*8], name='g_d2', with_w=True)
             d2 = self.g_bn_d2(self.d2)
+            print("##############d2 shape is {0}".format(d2.get_shape()))
             # d2 is (4 x 4 x self.gf_dim*8*2)
 
             self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
                 [self.batch_size, s32, s32, self.gf_dim*8], name='g_d3', with_w=True)
             d3 = self.g_bn_d3(self.d3)
+            print("##############d3 shape is {0}".format(d3.get_shape()))
             # d3 is (8 x 8 x self.gf_dim*8*2)
 
             self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
                 [self.batch_size, s16, s16, self.gf_dim*8], name='g_d4', with_w=True)
             d4 = self.g_bn_d4(self.d4)
+            print("##############d4 shape is {0}".format(d4.get_shape()))
             # d4 is (16 x 16 x self.gf_dim*8*2)
 
             self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
                 [self.batch_size, s8, s8, self.gf_dim*4], name='g_d5', with_w=True)
             d5 = self.g_bn_d5(self.d5)
+            print("##############d5 shape is {0}".format(d5.get_shape()))
             # d5 is (32 x 32 x self.gf_dim*4*2)
 
             self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
                 [self.batch_size, s4, s4, self.gf_dim*2], name='g_d6', with_w=True)
             d6 = self.g_bn_d6(self.d6)
+            print("##############d6 shape is {0}".format(d6.get_shape()))
             # d6 is (64 x 64 x self.gf_dim*2*2)
 
             return tf.nn.relu(self.d6)
