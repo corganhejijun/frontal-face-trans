241a7661ee7a8dfdc00c123b6aebb8bb8b4f5c53
corganhejijun <corganhejijun@gmail.com>
2018-12-03 16:11:42 +0800

    generate 64 and 128 test sample

--- src/pix2pix/model.py
+++ src/pix2pix/model.py
@@ -108,6 +108,8 @@ class pix2pix(object):
         self.D_256, self.D_logits_256 = self.discriminator_256(self.fake_AB_256, reuse=True)
 
         self.fake_B_sample = self.sampler(self.real_A_128)
+        self.fake_B_sample_64 = self.sampler_64(self.real_A_128)
+        self.fake_B_sample_128 = self.sampler_128(self.real_A_128)
 
         self.d64_sum = tf.summary.histogram("d64", self.D64)
         self.d128_sum = tf.summary.histogram("d128", self.D128)
@@ -564,6 +566,139 @@ class pix2pix(object):
                 [self.batch_size, s, s, self.output_c_dim], name='g_d8_256', with_w=True)
             # d8 is (256 x 256 x output_c_dim)
             return tf.nn.tanh(self.d8)
+            
+
+    def sampler_64(self, image, y=None):
+
+        with tf.variable_scope("generator") as scope:
+            scope.reuse_variables()
+
+            s = self.output_size
+            s4, s8, s16, s32, s64, s128 = int(s/4), int(s/8), int(s/16), int(s/32), int(s/64), int(s/128)
+
+            # image is (128 x 128 x input_c_dim)
+            e1 = conv2d(image, self.gf_dim, name='g_e1_conv_64')
+            # e1 is (64 x 64 x self.gf_dim)
+            e2 = self.g_bn_e2_64(conv2d(lrelu(e1), self.gf_dim*2, name='g_e2_conv_64'))
+            # e2 is (32 x 32 x self.gf_dim*2)
+            e3 = self.g_bn_e3_64(conv2d(lrelu(e2), self.gf_dim*4, name='g_e3_conv_64'))
+            # e3 is (16 x 16 x self.gf_dim*4)
+            e4 = self.g_bn_e4_64(conv2d(lrelu(e3), self.gf_dim*8, name='g_e4_conv_64'))
+            # e4 is (8 x 8 x self.gf_dim*8)
+            e5 = self.g_bn_e5_64(conv2d(lrelu(e4), self.gf_dim*8, name='g_e5_conv_64'))
+            # e5 is (4 x 4 x self.gf_dim*8)
+            e6 = self.g_bn_e6_64(conv2d(lrelu(e5), self.gf_dim*8, name='g_e6_conv_64'))
+            # e6 is (2 x 2 x self.gf_dim*8)
+            e7 = self.g_bn_e7_64(conv2d(lrelu(e6), self.gf_dim*8, name='g_e7_conv_64'))
+            # e7 is (1 x 1 x self.gf_dim*8)
+
+            self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e7),
+                [self.batch_size, s128, s128, self.gf_dim*8], name='g_d1_64', with_w=True)
+            d1 = tf.nn.dropout(self.g_bn_d1_64(self.d1), 0.5)
+            d1 = tf.concat([d1, e6], 3)
+            # d1 is (2 x 2 x self.gf_dim*8*2)
+
+            self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
+                [self.batch_size, s64, s64, self.gf_dim*8], name='g_d2_64', with_w=True)
+            d2 = tf.nn.dropout(self.g_bn_d2_64(self.d2), 0.5)
+            d2 = tf.concat([d2, e5], 3)
+            # d2 is (4 x 4 x self.gf_dim*8*2)
+
+            self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
+                [self.batch_size, s32, s32, self.gf_dim*8], name='g_d3_64', with_w=True)
+            d3 = tf.nn.dropout(self.g_bn_d3_64(self.d3), 0.5)
+            d3 = tf.concat([d3, e4], 3)
+            # d3 is (8 x 8 x self.gf_dim*8*2)
+
+            self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
+                [self.batch_size, s16, s16, self.gf_dim*8], name='g_d4_64', with_w=True)
+            d4 = self.g_bn_d4_64(self.d4)
+            d4 = tf.concat([d4, e3], 3)
+            # d4 is (16 x 16 x self.gf_dim*8*2)
+
+            self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
+                [self.batch_size, s8, s8, self.gf_dim*4], name='g_d5_64', with_w=True)
+            d5 = self.g_bn_d5_64(self.d5)
+            d5 = tf.concat([d5, e2], 3)
+            # d5 is (32 x 32 x self.gf_dim*4*2)
+
+            self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
+                [self.batch_size, s4, s4, self.output_c_dim], name='g_d6_64', with_w=True)
+            d6 = self.g_bn_d6_64(self.d6)
+            # d6 is (64 x 64 x self.gf_dim*2*2)
+
+            return tf.nn.tanh(d6)
+            
+
+    def sampler_128(self, image, y=None):
+
+        with tf.variable_scope("generator") as scope:
+            scope.reuse_variables()
+
+            s = self.output_size
+            s4, s8, s16, s32, s64, s128 = int(s/4), int(s/8), int(s/16), int(s/32), int(s/64), int(s/128)
+
+            # image is (128 x 128 x input_c_dim)
+            e1 = conv2d(image, self.gf_dim, name='g_e1_conv_64')
+            # e1 is (64 x 64 x self.gf_dim)
+            e2 = self.g_bn_e2_64(conv2d(lrelu(e1), self.gf_dim*2, name='g_e2_conv_64'))
+            # e2 is (32 x 32 x self.gf_dim*2)
+            e3 = self.g_bn_e3_64(conv2d(lrelu(e2), self.gf_dim*4, name='g_e3_conv_64'))
+            # e3 is (16 x 16 x self.gf_dim*4)
+            e4 = self.g_bn_e4_64(conv2d(lrelu(e3), self.gf_dim*8, name='g_e4_conv_64'))
+            # e4 is (8 x 8 x self.gf_dim*8)
+            e5 = self.g_bn_e5_64(conv2d(lrelu(e4), self.gf_dim*8, name='g_e5_conv_64'))
+            # e5 is (4 x 4 x self.gf_dim*8)
+            e6 = self.g_bn_e6_64(conv2d(lrelu(e5), self.gf_dim*8, name='g_e6_conv_64'))
+            # e6 is (2 x 2 x self.gf_dim*8)
+            e7 = self.g_bn_e7_64(conv2d(lrelu(e6), self.gf_dim*8, name='g_e7_conv_64'))
+            # e7 is (1 x 1 x self.gf_dim*8)
+
+            self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e7),
+                [self.batch_size, s128, s128, self.gf_dim*8], name='g_d1_64', with_w=True)
+            d1 = tf.nn.dropout(self.g_bn_d1_64(self.d1), 0.5)
+            d1 = tf.concat([d1, e6], 3)
+            # d1 is (2 x 2 x self.gf_dim*8*2)
+
+            self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
+                [self.batch_size, s64, s64, self.gf_dim*8], name='g_d2_64', with_w=True)
+            d2 = tf.nn.dropout(self.g_bn_d2_64(self.d2), 0.5)
+            d2 = tf.concat([d2, e5], 3)
+            # d2 is (4 x 4 x self.gf_dim*8*2)
+
+            self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
+                [self.batch_size, s32, s32, self.gf_dim*8], name='g_d3_64', with_w=True)
+            d3 = tf.nn.dropout(self.g_bn_d3_64(self.d3), 0.5)
+            d3 = tf.concat([d3, e4], 3)
+            # d3 is (8 x 8 x self.gf_dim*8*2)
+
+            self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
+                [self.batch_size, s16, s16, self.gf_dim*8], name='g_d4_64', with_w=True)
+            d4 = self.g_bn_d4_64(self.d4)
+            d4 = tf.concat([d4, e3], 3)
+            # d4 is (16 x 16 x self.gf_dim*8*2)
+
+            self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
+                [self.batch_size, s8, s8, self.gf_dim*4], name='g_d5_64', with_w=True)
+            d5 = self.g_bn_d5_64(self.d5)
+            d5 = tf.concat([d5, e2], 3)
+            # d5 is (32 x 32 x self.gf_dim*4*2)
+
+            self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
+                [self.batch_size, s4, s4, self.output_c_dim], name='g_d6_64', with_w=True)
+            d6 = self.g_bn_d6_64(self.d6)
+            # d6 is (64 x 64 x self.gf_dim*2*2)
+
+            out_64 = tf.nn.tanh(d6)
+
+            rb1 = self.residual_block_1_128(out_64)
+            rb2 = self.residual_block_2_128(rb1)
+            s2 = int(s/2)
+            self.d7, self.d7_w, self.d7_b = deconv2d(rb2,
+                [self.batch_size, s2, s2, self.output_c_dim], name='g_d7_128', with_w=True)
+            d7 = self.g_bn_d7_128(self.d7)
+            # d7 is (128 x 128 x self.gf_dim*1*2)
+            return tf.nn.tanh(d7)
 
     def save(self, checkpoint_dir, step):
         model_name = "pix2pix.model"
@@ -641,3 +776,15 @@ class pix2pix(object):
                 )
                 save_images(samples, [self.batch_size, 1],
                             './{}/{}.png'.format(args.test_dir, fileName))
+                samples = self.sess.run(
+                    self.fake_B_sample_64,
+                    feed_dict={self.real_data: sample_image}
+                )
+                save_images(samples, [self.batch_size, 1],
+                            './{}_64/{}.png'.format(args.test_dir, fileName))
+                samples = self.sess.run(
+                    self.fake_B_sample_128,
+                    feed_dict={self.real_data: sample_image}
+                )
+                save_images(samples, [self.batch_size, 1],
+                            './{}_128/{}.png'.format(args.test_dir, fileName))
